663a06e9e948ae6ac63b65d4fff93f6d21e5ed0a
Eliz30ttt <elizwilson321@gmail.com>
2026-02-20 10:09:17 -0800

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,16 +1,12 @@
 import { auth } from "./firebase.js";
-import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
 const logoutBtn = document.getElementById("logoutBtn");
-
 const ADMIN_EMAIL = "admin@novabank.com";
 
 onAuthStateChanged(auth, (user) => {
-  if (!user) {
+  if (!user || user.email !== ADMIN_EMAIL) {
     window.location.href = "login.html";
-  } else if (user.email !== ADMIN_EMAIL) {
-    alert("Access denied.");
-    window.location.href = "dashboard.html";
   }
 });
 
