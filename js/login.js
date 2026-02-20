c00277a569d97600ce77aa324b93a4062eea62d5
Eliz30ttt <elizwilson321@gmail.com>
2026-02-20 06:50:58 -0800

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,19 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+const form = document.getElementById("loginForm");
+const errorMessage = document.getElementById("errorMessage");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "dashboard.html";
+  } catch (error) {
+    errorMessage.textContent = error.message;
+  }
+});
